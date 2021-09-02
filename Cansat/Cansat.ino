c6e0fc479a6cead130654090abae8ed9e7c4f276
Jakub Andrlík <andrlikjakub@seznam.cz>
2021-09-02 21:14:45 +0200

    fun. to read data from bme and acc
    Signed-off-by: Jakub Andrlík <andrlikjakub@seznam.cz>

---
+++ Cansat/Cansat.ino
@@ -0,0 +1,162 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_MPU6050.h>
+
+
+/**** HC-12 serial port ****/
+#define RXD2 16  //(RX2)
+#define TXD2 4  //(TX2)
+
+#define HC12 Serial2  //Hardware serial 2 on the ESP32
+
+
+/**** BMP280 ****/
+Adafruit_BMP280 bme; // I2C
+float temp = 0;
+float pressure = 0;
+
+
+/**** AKCELEROMETR ****/
+Adafruit_MPU6050 mpu;
+
+
+
+/**** FUNCTIONS ****/
+bool sendData(String _data);    //send data to HC-12
+void readBME(float *_temp, float *_press);    //read bme data
+void getAccData(float* _x, float* _y, float* _z);   //read acceleration
+void getGyroData(float* _x, float* _y, float* _z);  //read rotation
+void getAccTemp(float* _temp);  //read acc. temp
+
+
+void setup() 
+{
+  
+  Serial.begin(115200);           // Serial port to computer
+  HC12.begin(9600, SERIAL_8N1, RXD2, TXD2);      // Serial port to HC12
+ 
+  if (!bme.begin(0x76)) {   //init bme280
+  Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+  while (1);
+  }
+
+  if (!mpu.begin()) {   //init accelerometer
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);   //set accelerometer
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  
+  Serial.write("INIT DONE");}
+
+
+void loop() 
+{
+
+
+  /*while (HC12.available()) 
+  {        
+    // If HC-12 has data
+    Serial.write(HC12.read());
+    // Send the data to Serial monitor
+    
+  }*/
+
+  readBME(&temp, &pressure);
+
+  Serial.print("Temperature = ");
+  Serial.print(temp);
+  Serial.println(" *C");
+   
+  Serial.print("Pressure = ");
+  Serial.print(pressure);
+  Serial.println(" Pa");
+
+
+  float acc_x, acc_y, acc_z = 0;
+
+  getAccData(&acc_x, &acc_y, &acc_z);
+
+  Serial.print("Acceleration X: ");
+  Serial.print(acc_x);
+  Serial.print(", Y: ");
+  Serial.print(acc_y);
+  Serial.print(", Z: ");
+  Serial.print(acc_z);
+  Serial.println(" m/s^2");
+
+
+  float g_x, g_y, g_z = 0;
+  
+  getGyroData(&g_x, &g_y, &g_z);
+
+  Serial.print("Rotation X: ");
+  Serial.print(g_x);
+  Serial.print(", Y: ");
+  Serial.print(g_y);
+  Serial.print(", Z: ");
+  Serial.print(g_z);
+  Serial.println(" rad/s");
+
+  float acc_temp = 0;
+  
+  getAccTemp(&acc_temp);
+
+  Serial.print("Temperature: ");
+  Serial.print(acc_temp);
+  Serial.println(" degC");
+
+  delay(2000);
+  
+}
+
+bool sendData(String _data){    //send data to hc-12
+
+  HC12.write(_data.c_str());  //Send data to HC-12
+
+}
+
+void readBME(float* _temp, float* _press){    //read values from bme280 and save it to variables
+
+  *_temp = bme.readTemperature();
+  *_press = bme.readPressure();
+  
+}
+
+void getAccData(float* _x, float* _y, float* _z){
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);;
+
+  *_x = a.acceleration.x;
+  *_y = a.acceleration.y;
+  *_z = a.acceleration.z;
+  
+}
+
+void getGyroData(float* _x, float* _y, float* _z){
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  *_x = g.gyro.x;
+  *_y = g.gyro.y;
+  *_z = g.gyro.z;
+  
+}
+
+void getAccTemp(float* _temp){
+
+  sensors_event_t a, g, t;
+  mpu.getEvent(&a, &g, &t);
+
+  *_temp = t.temperature;
+
+}
+
